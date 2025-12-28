6cc595a4313c111bfe08bfc3b79db671a8539d94
Tharun <tharun.wasantha@gmail.com>
2025-12-28 18:34:40 +0530

    BIT_VLE_SEM1 codes

---
+++ src/BorderLayoutDemo.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+class BorderLayoutDemo {
+        public static void main(String[] args) {
+            JFrame jf1 = new JFrame();
+            //jf1.setSize(400,400);
+            jf1.setBounds(300,300,400,400);
+            jf1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            jf1.setLayout(new BorderLayout());
+
+            JButton b1 = new JButton("NORTH");
+            JButton b2 = new JButton("SOUTH");
+            JButton b3 = new JButton("EAST");
+            JButton b4 = new JButton("WEST");
+            JButton b5 = new JButton("CENTER");
+
+            jf1.add(b1,BorderLayout.NORTH);
+            jf1.add(b2,BorderLayout.SOUTH);
+            jf1.add(b3,BorderLayout.EAST);
+            jf1.add(b4,BorderLayout.WEST);
+            jf1.add(b5,BorderLayout.CENTER);
+
+            jf1.setVisible(true);
+        }
+}
---
+++ src/MyFrameDemo.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+class MyFrameDemo {
+    public static void main(String[] args) {
+        JFrame jf1 = new JFrame();
+        //jf1.setSize(400,400);
+        jf1.setBounds(300,300,400,400);
+        jf1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf1.setVisible(true);
+    }
+}
