6449ec462bc9b9be1aa868a341b6c29e3e35d6cd
Tharun <tharun.wasantha@gmail.com>
2025-12-28 14:51:16 +0530

    BIT_VLE_SEM1 codes

--- src/Addition.java
+++ src/Addition.java
@@ -16,6 +16,7 @@ public class Addition{
 		System.out.println(sub);
 		System.out.println(mul);
 		System.out.println(div);
+		System.out.println(sum+sub+mul+div);
 
 		//Is equal to
 		System.out.println(num1 == num2);
